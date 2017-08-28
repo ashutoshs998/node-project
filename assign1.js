fb43ee764f5ef2996b8011fb384d669154b8ecc0
ashutosh_m <surendra@excellencetechnologies.in.com>
2017-08-28 18:32:35 +0530

    updated

--- assign1.js
+++ assign1.js
@@ -2,28 +2,26 @@ var mongoose = require('mongoose');
 var encrypt = require('md5');
 var async = require('async');
 mongoose.Promise = global.Promise;
-var connect = mongoose.createConnection('mongodb://localhost/mydb');
+var conn = mongoose.createConnection('mongodb://localhost/last');
 var user_schema = mongoose.Schema({
     firstname: String,
     lastname: String,
     email: String,
     password: String
-}, 
-{
+}, {
     strict: true,
     collection: 'Users'
 });
-var proschema = mongoose.Schema({
+var userprofile_schema = mongoose.Schema({
     User_id: String,
     dob: String,
     ph_no: Number
-}, 
-{
+}, {
     strict: true,
     collection: 'UsersProfile'
 });
-var user = connect.model('user', user_schema);
-var userprofile = connect.model('userprofile', proschema);
+var user = conn.model('user', user_schema);
+var userprofile = conn.model('userprofile', userprofile_schema);
 var detail = [
     { firstname: 'ashutosh', lastname: 'mudgal', email: 'ashu@gmail.com', password: 'dgjlflks', dob: '02/02/1993', ph_no: 9842382328 },
     { firstname: 'abhi', lastname: 'sri', email: 'abhi@gmail.com', password: 'djfkskd', dob: '03/03/2000', ph_no: 9292392392 },
@@ -32,25 +30,23 @@ var detail = [
     { firstname: 'sumit', lastname: 'sri', email: 'ashu@gmail.com', password: 'rlskgrgsd', dob: '06/06/1998', ph_no: 9223828399 },
     { firstname: 'saurbh', lastname: 'k', email: 'saurabh@gmail.com', password: 'saurhdhsh', dob: '07/07/2001', ph_no: 8032723727 },
 ];
-async.forEachOf(detail, function(udata, key, callback) {
-        var userdata = new user({
-            firstname: udata.firstname,
-            lastname: udata.lastname,
-            email: udata.email,
-            password: encrypt(udata.password)
+async.forEachOf(detail, function(user_collection_data, key, callback) {
+        var user_data = new user({
+            firstname: user_collection_data.firstname,
+            lastname: user_collection_data.lastname,
+            email: user_collection_data.email,
+            password: encrypt(user_collection_data.password)
         });
-        userdata.save(function(err, pdata) {
+        user_data.save(function(err, userprofile_collection_data) {
             if (err) {
                 console.log(err);
-            } 
-            else if (pdata) {
-                var prodata = new userprofile
-	                ({
-	                    User_id: pdata._id,
-	                    dob: udata.dob,
-	                    ph_no: udata.ph_no
-	                });
-                prodata.save(function(err, udata) {
+            } else if (userprofile_collection_data) {
+                var userprofile_data = new userprofile({
+                    User_id: userprofile_collection_data._id,
+                    dob: user_collection_data.dob,
+                    ph_no: user_collection_data.ph_no
+                });
+                userprofile_data.save(function(err, user_collection_data) {
                     if (err) {
                         console.log(err);
                     }
