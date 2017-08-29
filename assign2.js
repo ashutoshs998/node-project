929bfef726a12d7d1bf43dcfbff24b188c542e38
ashutosh_m <surendra@excellencetechnologies.in.com>
2017-08-29 16:15:44 +0530

    assignment 2

---
+++ assign2.js
@@ -0,0 +1,35 @@
+var MongoClient = require('mongodb').MongoClient;
+var average = 0;MongoClient.connect('mongodb://localhost/last', function(err, db, callback) {
+    if (err) {
+        console.log(err);
+    }
+    var present_date = new Date();
+    var sum = 0,
+        count = 0;
+    db.collection('UsersProfile').find().each(function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+        if (data) {
+            var date_of_birth = new Date(data.dob);
+            var age = present_date.getFullYear() - date_of_birth.getFullYear();
+            sum = sum + age;
+            count++;
+            if (age >= 25) {
+                console.log("above age is more than 25 years")
+                db.collection('UsersProfile').remove({ "_id": data._id }, function(err, result) {
+                    if (result === 1) {
+                        console.log('error: ' + err)
+                    } else {
+                        console.log('data with the age over 25 is Deleted')
+                    };
+                });
+            }
+            average = sum / count;
+        } else {
+            console.log("Average age: " + average);
+        }
+    });
+    if (callback)
+        callback();
+});
